8eb207516ee643604adbd76f0db4a02fe9ea88d0
cliff <cliffmudigo5@gmail.com>
2023-12-04 10:55:29 +0300

    :sparkles: modifying the code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,15 +11,54 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+
 
 app.get("/", function(req, res) {
 
 
 
-  res.render("list", {listTitle: day, newListItems: items});
+  Item.find({})
+  .then(function(foundItems){
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems)
+.then(function (){
+  console.log("Successfully saved default items to DB.");
+}).catch(function(err){
+  console.log(err);
+});
+    }else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+    
+  })
+  .catch(function(err){
+    console.log(err);
+  })
 
+
+  
 });
 
 app.post("/", function(req, res){
